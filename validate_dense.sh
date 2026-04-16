c1af59047aa28b766e1b2b9dd79e8f602a568f5c
Adr <adbanachowicz@gmail.com>
2026-04-16 23:34:59 +0200

    DUKE-16: Add Validate Dense sh

--- validate_dense.sh
+++ validate_dense.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --job-name=dense
+#SBATCH --job-name=validate_dense
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
@@ -15,6 +15,7 @@ ml ML-bundle
 
 cd /net/home/plgrid/plgabanacho/mri_brest
 pip install -e .
-pip install --no-cache-dir pytorch_lightning lightning==2.5.6 torchmetrics==1.8.2 requests monai pandas scikit-learn nibabel openpyxl tensorboardX einops matplotlib
+pip install --no-cache-dir pytorch_lightning lightning==2.5.6 torchmetrics==1.8.2 requests monai pandas scikit-learn nibabel openpyxl tensorboardX einops
 
-python -m mriBreastDuke.trainingWorkflow --model 1 --epoch 30
+python -m mriBreastDuke.validate_best_checkpoints --model 1
+
